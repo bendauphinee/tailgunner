f34e694dc8e3c34753515e37f26a311119ac5408
Ben Dauphinee <me@bendauphinee.com>
2024-12-21 16:48:43 -0400

    Initial version of Laravel, with slight tweaks to how Vite handles host address

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://tailgunner.app/tailgunnerlogo.png" width="400" alt="Laravel Logo"></a></p>
+
+## About Install
+
+Laravel 11 / PHP 8.4
---
+++ src/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({ mode }) => {
+    // Load env file based on mode
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        server: {
+            host: env.VITE_APP_ADDRESS
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    }
+});
