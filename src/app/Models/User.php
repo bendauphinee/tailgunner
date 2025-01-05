837ef16a6bf9750cf2c83f40b17149d55cdf78b6
Ben Dauphinee <me@bendauphinee.com>
2025-01-05 10:38:06 -0400

    Updated user model to add templates hook

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -66,4 +66,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function templates()
+    {
+        return $this->hasMany(Template::class);
+    }
 }
