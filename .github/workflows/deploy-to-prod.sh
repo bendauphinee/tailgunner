5c7abfdf1007b9f08efd4ac9dc76f7994609df62
Ben Dauphinee <me@bendauphinee.com>
2025-01-15 17:05:53 -0400

    US1-C4 Updated deploy scripts

--- .github/workflows/deploy-to-prod.sh
+++ .github/workflows/deploy-to-prod.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Take username as argument
+USERNAME=$1
+
 # Set deployment directories
-DEPLOY_PATH=/var/www/html
-RELEASE_PATH=/var/www/releases/$(date +%Y%m%d_%H%M%S)
-SHARED_PATH=/var/www/shared
+DEPLOY_PATH=/home/$USERNAME/public_html
+RELEASE_PATH=/home/$USERNAME/releases/$(date +%Y%m%d_%H%M%S)
+SHARED_PATH=/home/$USERNAME/shared
 
 # Create required directories
 mkdir -p $RELEASE_PATH
@@ -24,9 +27,15 @@ ln -s $SHARED_PATH/storage $RELEASE_PATH/storage
 chmod -R 755 $RELEASE_PATH
 chown -R www-data:www-data $RELEASE_PATH
 
+# Refresh Laravel caches
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan storage:link
+
 # Activate new release
 ln -sfn $RELEASE_PATH $DEPLOY_PATH
 
 # Clean up old releases (keep last 5)
-cd /var/www/releases
+cd /home/$USERNAME/releases
 ls -1dt */ | tail -n +6 | xargs -d '\n' rm -rf --
