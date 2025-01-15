ba586ed10c1606e1171c38c2dd6479abd4fd946e
Ben Dauphinee <me@bendauphinee.com>
2025-01-15 18:33:39 -0400

    US1-C4 More changes to see what's wrong with Vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,28 +1,20 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ mode }) => {
-    // Load env file based on mode
-    const env = loadEnv(mode, process.cwd());
-
-    return {
-        server: {
-            host: env.VITE_APP_ADDRESS
-        },
-        plugins: [
-            laravel({
-                input: 'resources/js/app.js',
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-    }
+            },
+        }),
+    ],
 });
