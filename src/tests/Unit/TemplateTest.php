7811dfd8132fced8435ffa1f00636aaad2e02c6e
Ben Dauphinee <me@bendauphinee.com>
2025-01-24 16:26:07 -0400

    Added some tests

--- src/tests/Unit/TemplateTest.php
+++ src/tests/Unit/TemplateTest.php
@@ -11,6 +11,22 @@ class TemplateTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_can_create_template()
+    {
+        $templateData = [
+            'title' => 'Test Template',
+            'description' => 'Test Description',
+            'user_id' => 1
+        ];
+
+        $template = Template::create($templateData);
+
+        $this->assertDatabaseHas('templates', $templateData);
+        $this->assertEquals($templateData['title'], $template->title);
+        $this->assertEquals($templateData['description'], $template->description);
+        $this->assertEquals($templateData['user_id'], $template->user_id);
+    }
+
     public function test_template_and_fields_can_be_loaded()
     {
         $template = Template::factory()->create();
